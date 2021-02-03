73791a1dff6ef2f5c80c3acba6d0ab9a09357767
Audrey Dhalluin <dhalluinaud@gmail.com>
2021-02-03 09:58:59 +0100

    init front

---
+++ front/html/Option1.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<meta charset="utf-8">
+	<!--  Liens Boostrap -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link href="http://fonts.googleapis.com/css?family=Cookie" rel="stylesheet" type="text/css">
+	<link rel="stylesheet" href="Accueil.css"> <!-- Pour le CSS de la page -->
+
+			<title>Stratego</title>
+
+</head>
+
+	<body>
+
+		<ul id="menu">
+
+	      <li><a href="Accueil.php">Accueil</a></li>
+
+			  <li><a href = " Option1.php " > Option 1</a></li>
+ 		  	<li><a href = " Option2.php " >Option 2</a></li>
+
+			  <li><a href="#">Jouer !</a>
+			 			 <ul>
+			 			 <li><a href = "1v1.php"> 1v1 </a></li>
+			 			 </ul>
+	 </li>
+	 </ul>
+	 <div class="main"> Option 1 </div>
+</body>
+							<!-- ************** Pour le footer ************  -->
+<footer class="footer-distributed">
+					<div class="footer-left">
+							<h3>Jouez et <span>Gagnez</span></h3>
+							<p class="footer-words">
+								<a href="#">Facile</a> · <a href="#">Multijoueur</a> · <a href="#">Classique</a> · <a href="#">Amusant</a></p>
+							<p class="footer-company-name">Le Fameux Groupe 5 &copy; Projet.2021</p>
+					</div>
+
+					<div class="footer-center">
+							<div>
+									<i class="fa fa-map-marker"></i>
+									<p><span>ISEN Digital</span> Lille, France</p>
+							</div>
+							<div>
+									<i class="fa fa-envelope"></i>
+									<p><a href="mailto:support@company.com">YaPasDeMail@projet.com</a></p>
+							</div>
+					</div>
+
+					<div class="footer-right">
+							<p class="footer-company-about">
+									<span>A propos de nous:</span>
+									Stratego est un site dont le but est de jouer au stratego classique mais avec la possibilité de jouer contre un vrai joueur. Ce site a été créé pour un projet par des élèves de l'Isen.
+							</p>
+							<div class="footer-icons">
+									<a href="#"><i class="fa fa-facebook"></i></a>
+									<a href="#"><i class="fa fa-twitter"></i></a>
+									<a href="#"><i class="fa fa-linkedin"></i></a>
+									<a href="#"><i class="fa fa-github"></i></a>
+							</div>
+					</div>
+
+		</footer>
+
+	</body>
+
+</html>
