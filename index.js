72306285aebbcd608169e9ce110a2c30319a1431
biiskot <gabriel.contois@gmail.com>
2021-02-03 10:06:51 +0100

    socketio messages

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ const io = require('socket.io')(http);
 app.use(express.static(__dirname+'/front'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'front/html/index.html');
+    res.sendFile(__dirname + '/front/html/index.html');
 });
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
 });
 
+//user
 io.on('connection', (socket) => {
     console.log('a user connected');
 });
+
+
+//message cote server
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      console.log('message: ' + msg);
+    });
+  });
+
+  //broadcast cote front
+  socket.on('message',(msg)=>{
+    console.log('message: '+msg)
+    io.emit('new-message',msg)
+  });
+
+  //voir docu socket.io
