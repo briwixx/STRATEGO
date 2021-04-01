414eba73dabb1ec0bafae6ea640f11982356f962
biiskot <gabriel.contois@gmail.com>
2021-04-01 11:44:57 +0200

    add classPlateau.js

---
+++ back/game files/classPlateau.js
@@ -0,0 +1,33 @@
+
+class Plateau {
+  constructor(lignes,colonnes) {
+    this.check1 = false;
+    this.check2 = false;
+    this.lignes = lignes;
+    this.colonnes = colonnes;
+
+    //Initialisation d'un plateau lignes x colonnes :
+    this.plateau = new Array(this.lignes);
+
+    for (let i = 0 ; i < this.plateau.length ; i++){
+      this.plateau[i] = new Array(this.colonnes);
+    }
+
+    //Remplissage tableau avec des 1 :
+    for(let i = 0 ; i < this.lignes ; i++){
+      for (let j = 0; j < this.colonnes ; j++){
+        this.plateau[i][j] = 1;
+      }
+    }
+    //Création des 2 lacs 4x4 avec des 0:
+    for (let i = 0 ; i < 2 ; i++){
+      this.plateau[4+i][2] = 0;
+      this.plateau[4+i][6] = 0;
+      for (let j = 0 ; j < 2 ; j++){
+        this.plateau[4+i][3] = 0;
+        this.plateau[4+i][7] = 0;
+      }
+    }
+  }
+}
+let plateau = new Plateau(10,10);
